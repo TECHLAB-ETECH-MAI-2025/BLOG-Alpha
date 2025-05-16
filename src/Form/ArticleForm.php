09480a74525664d01c3ad875d501c2fd0871923d
alpha592 <alpharasoaniriana@gmail.com>
2025-05-16 11:12:16 +0200

    personnalisation des divers pages comme ArticleForm.php,form.html.twig et new.html.twig des articles

--- src/Form/ArticleForm.php
+++ src/Form/ArticleForm.php
@@ -8,18 +8,47 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ArticleForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('content')
+            ->add('title', TextType::class,[
+                'label' => 'Titre de l\'article',
+                'attr'  =>[
+                    'class'=> 'form-control',
+                    'placeholder' => 'Entrez un titre pour votre article',
+					'required' => true,
+                    'style' => 'margin: 10px',
+                ]
+            ])
+
+            ->add('content',TextType::class,[
+                'label' => 'Que contient votre article',
+                'attr'  => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Ecrivez tout ce que vous voulez a propos de votre article ici',
+                    'style' => ' height: 100px; margin: 10px',
+                    'required' => true,
+                ]
+            ])
+
             ->add('categories', EntityType::class, [
                 'class' => Category::class,
+                'label' => 'Catégories',
                 'choice_label' => 'title',
                 'multiple' => true,
+                'expanded' => true,
+                'attr' => [
+					'class' => 'form-check',
+                    'style' => 'display: flex; flex-direction: column;'
+                ],
+                'label_attr' => [
+						'class' => 'form-check-label'
+					]            
             ])
         ;
     }
